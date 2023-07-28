e7f705ca6b7ffea2911a9cf3a280162ceb85945c
Nico <nicolas.gastellu@mail.mcgill.ca>
2023-07-28 16:28:49 -0400

    Optimised memory performance of LAMMPS dump file parsing. Added `rerun_LUMO_arpack_MAC` to `QuickArpackBigMAC` which fixes the issue of non-converging LUMO energy estimation.

--- CoordsIO.jl
+++ CoordsIO.jl
@@ -1,24 +1,43 @@
 module CoordsIO
 
 export read_xsf, get_frame
-
-function read_xsf(filename; read_forces=true)
+function read_xsf(filename; read_forces=false, dump=false)
+    
     f = open(filename)
-    for i in 1:2
-        readline(f)
+    if dump
+        for i=1:3
+            readline(f)
+        end
+        
+        na = parse(Int, split(readline(f))[1])
+        
+        for i=1:5
+            readline(f)
+        end
+    
+    else
+        for i in 1:2
+            readline(f)
+        end
+
+        supercell = []
+        push!(supercell, parse(Float64, split(readline(f))[1]))
+        push!(supercell, parse(Float64, split(readline(f))[2]))
+
+        for i in 1:2
+            readline(f)
+        end
+
+        na = parse(Int, split(readline(f))[1])
+
     end
 
-    supercell = []
-    push!(supercell, parse(Float64, split(readline(f))[1]))
-    push!(supercell, parse(Float64, split(readline(f))[2]))
-
-    for i in 1:2
-        readline(f)
-    end
-
-    na = parse(Int, split(readline(f))[1])
     atoms = zeros(Float64, (na, 3))
-    forces = zeros(Float64, (na, 3))
+
+    if read_forces
+        forces = zeros(Float64, (na, 3))
+    end
+    
     forces_in_file = false
 
     for k in 1:na
@@ -34,54 +53,45 @@ function read_xsf(filename; read_forces=true)
 
     close(f)
 
-    if forces_in_file && read_forces
-        return atoms, forces, supercell
+    if dump
+        return atoms
     else
-        return atoms, supercell
+        if forces_in_file && read_forces
+            return atoms, forces, supercell
+        else
+            return atoms, supercell
+        end
     end
 end
 
 
-function get_frame(filename, frame_index)
-
-    nb_non_coord_lines::Int = 9
+function get_Natoms_dump(filename)
     fo = open(filename)
-    
-    for i=1:3
-        readline(fo)
-    end
-
+    nchars = 39 #number of chars to read before reaching line containing the number of atoms (assuming file is a dump file ⟺ line 1 = 'ITEM: TIMESTEP\n')
+    seek(fo,nchars)
     Natoms = parse(Int, readline(fo))
-
-    pos = zeros(Float64, (Natoms,3))
-
-    nlines_per_frame = Natoms + nb_non_coord_lines
-
-    seekstart(fo)
-
-    for i=0:frame_index-1
-        for j=1:nlines_per_frame
-            readline(fo)
-        end
-    end
-
-    for i=1:nb_non_coord_lines
-        l = readline(fo)
-        if i <= 2
-            println(l)
-        end
-    end
-
-    for k in 1:Natoms
-        split_line = split(readline(fo))
-        x, y, z = split_line[2:4]
-        pos[k, :] = [parse(Float64, x), parse(Float64, y), parse(Float64, z)]
-    end
-
     close(fo)
+    return Natoms
+end
+
+function get_frame(filename, frame_index; frame_step=1, frame0_index=0)
+    
+    nb_non_coord_lines::Int = 9
+    Natoms = get_Natoms_dump(filename)
+    nlines_per_frame = Natoms + nb_non_coord_lines
+    nlines_tail = nlines_per_frame * (Int((frame_index - frame0_index)/frame_step) + 1)
+
+    pos = zeros(Float64, (Natoms, 3))
+    rawpos = read(pipeline(`head -n $nlines_tail $filename`, `tail -n $Natoms`), String) #this step is the bottleneck... might need to optimise
+    allpos = split(rawpos, '\n')
+
+    for i=1:Natoms
+        x, y, z = split(allpos[i])[2:4]
+        pos[i,:] = [parse(Float64, x), parse(Float64, y), parse(Float64, z)]
+    end
 
     return pos
-
+    
 end
-
+    
 end
