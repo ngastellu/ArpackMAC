393d480ad17ba7878fab46f1ad2f21c4a014a571
Nico <nicolas.gastellu@mail.mcgill.ca>
2024-02-04 02:54:59 -0500

    Refined `CheckBandGaps.jl`

--- to_narval/CheckBandGaps.jl
+++ to_narval/CheckBandGaps.jl
@@ -1,22 +1,20 @@
 module CheckBandGaps
 
     include("./SpectralLanczos.jl")
-    include("./TightBinding.jl")
-    include("./CoordsIO.jl")
     
-    using .SpectralLanczos, .TightBinding, .CoordsIO, PyCall, Base.Filesystem
+    using .SpectralLanczos,  PyCall, Base.Filesystem, SparseArrays
 
     py"""
     import numpy as np
     vir_lbls = np.load('vir_lbls.npy')
     """
 
-    lbls = PyArray(py"vir_lbls"o)[1:2]
+    lbls = PyArray(py"vir_lbls"o)
     rCC = 1.8
 
     ii = parse(Int, ARGS[1])
     n = lbls[ii]
-    Δn = zeros(Int, 2)
+  
     
     println("**** $n ****")
     eocc_file = "energies/occupied/eARPACK_bigMAC-$n.npy"
@@ -25,35 +23,77 @@ module CheckBandGaps
     evirt_file = "energies/virtual/eARPACK_bigMAC-$n.npy"
     evirts = sort(PyArray(py"np.load($evirt_file)"o))
 
-    posfile = expanduser("~/scratch/clean_bigMAC/40x40/relax/no_PBC/relaxed_structures/bigMAC-$(n)_relaxed.xsf") 
-    pos, _ = read_xsf(posfile;read_forces=false)
-    py"""
-    from qcnico.remove_dangling_carbons import remove_dangling_carbons
-    rCC = $rCC
-    pos = remove_dangling_carbons($(PyObject(pos)),rCC)
-    """
-    pos = PyArray(py"pos"o)
-    
+    natoms_file = "natoms.npy"
+    natoms = PyArray(py"np.load($natoms_file)"o)
+
+    ii_file = "hamiltonians/inds/ii-$n.npy"
+    iii = PyArray(py"np.load($ii_file)"o)
+
+    jj_file = "hamiltonians/inds/jj-$n.npy"
+    jj = PyArray(py"np.load($jj_file)"o)
+
+    hvals_file = "hamiltonians/hvals/hvals-$n.npy"
+    hvals = PyArray(py"np.load($hvals_file)"o)
+
+    N = natoms[ii]
+ 
     println("Constructing hamiltonian...")
-    H, ii, jj, hvals = lindbergHtb_sparse(pos,rCC;return_data=true)
+    H = sparse(iii,jj,hvals,N,N)
+    H += H'
     println("Done!")
 
-    N = size(H,1)
-    nhalf = Int(floor(N/2))
 
-    eHOMO = eoccs[end]
-    eLUMO = evirts[1]
+    if N % 2 == 0
 
-    Δn[k,1], _ = count_evals(H, eHOMO)
-    Δn[k,2], _ = count_evals(H, eLUMO)
+        nhalf = Int(floor(N/2))
+
+        k_occ = 0
+        k_virt = 1
+
+        eocc = eoccs[end-k_occ]
+        evirt = evirts[k_virt]
+
+        occ_Δn = zeros(Int, size(eoccs,1))
+        virt_Δn = zeros(Int, size(evirts,1),)
+
+        δn_occ, _ = count_evals(H, eocc)
+        δn_virt, _ = count_evals(H, evirt)
+
+        if δn_occ < nhalf - 1 && δn_vir > nhalf + 1
+            println("Bad structure:")
+            println("N/2 = $nhalf")
+            println("δn_occ = $δn_occ")
+            println("δn_virt = $δn_virt")
+
+        else
+            occ_Δn[k_occ+1] = δn_occ
+            virt_Δn[k_virt] = δn_virt
+
+            if δn_occ > nhalf - 1
+                k_occ +=1
+                eocc = eoccs[end-k_occ]
+                δn_occ , _= count_evals(H,eocc)
+                occ_Δn[k_occ+1] = δn_occ
+            end
+
+            if δn_virt < nhalf 
+                k_virt +=1
+                evirt = evirts[k_virt]
+                δn_virt, _ = count_evals(H,evirt)
+                virt_Δn[k_virt] = δn_virt
+            end
 
 
-    py"""
-    nn = $n
-    np.save(f'gap_check/dN-{nn}.npy', $(PyObject(Δn)))
-    np.save(f'hamiltonians/inds/ii-{nn}.npy', $(PyObject(ii)))
-    np.save(f'hamiltonians/inds/jj-{nn}.npy', $(PyObject(jj)))
-    np.save(f'hamiltonians/hvals/hvals-{nn}.npy', $(PyObject(hvals)))
-    """
+            py"""
+            nn = $n
+            np.save(f'gap_check/occupied/odN-{nn}.npy', $(PyObject(occ_Δn)))
+            np.save(f'gap_check/virtual/vdN-{nn}.npy', $(PyObject(virt_Δn)))
+            """
+        end
+
+    else
+
+        println("Odd number of atoms: N = $N")
+    end
 
 end
