c6daeaee0ff430074aa876a7ca2e12a5a0db3bd8
Nico <nicolas.gastellu@mail.mcgill.ca>
2024-02-28 18:17:47 -0500

    added `remove_redundant_MOs` to `utils_arpacMAC` (previously `utils_arpack`), added this functionality to `group_eigenpairs`

--- utils_arpackMAC.py
+++ utils_arpackMAC.py
@@ -48,19 +48,53 @@ def find_redundant_MOs(e1,e2,M1,M2,e_eps=1e-4,M_eps=1e-5):
         M2j = M2[:,jj]
         inner_products = np.abs((M1i @ M2j).diagonal())
         
-        isame = (inner_products > 1-M_eps).nonzero()
-        if len(isame) > 0:
+        isame = (np.abs(inner_products - 1) < M_eps).nonzero()
+        if len(isame[0]) > 0:
             return ii[isame], jj[isame]
     return [[],[]]
 
-def gen_grouped_eigenpairs(edirs,Mdirs,lbls,efilename_templates, Mfilename_templates):
+def remove_redundant_eigenpairs(e,M,e_eps=1e-6,M_eps=1e-6):
+    """Given a list of eigenpairs (e,M) with potential redundancies (e.g. bc list of eigenpairs is generated
+    by concatenating eigenpairs from separate runs), return the shortened list of eigenpairs (e',M') without
+    the redundancies. The redundant eigenpairs are found using the method as `find_redundant_MOs`."""
+    dE = e[:,None] - e
+    ij = np.vstack(np.abs(dE < e_eps).nonzero())
+    ij = ij[(ij[0,:] - ij[1,:] > 0)] # remove self-differences and equivalent differences
+
+    if ij.shape[1] > 0:
+        print(f'!! Found {ij.shape[1]} energy differences smaller than {e_eps} !!', flush=True)
+        Mi = M[:,ij[0]].T
+        Mj = M[:,ij[1]]
+        inner_products = np.abs((Mi @ Mj).diagonal())
+        isame = (np.abs(inner_products - 1) < M_eps).nonzero()
+        if len(isame[0]) > 0:
+            print(f'*!!! Found {len(isame[0])} pairs of MOs with inner products within {M_eps} of 1 !!!*', flush=True)
+            iredundant = set(ij[isame,1]) # indices of redundant eigenpairs
+            all_inds = set(range(e.shape[0]))
+            ikeep = np.array(list(all_inds - iredundant))
+            e = e[ikeep]
+            M = M[:,ikeep]
+    print('Now sorting energies and MOs...', flush=True)
+    isorted = np.argsort(e)
+    e = e[isorted]
+    M = M[:,isorted]
+    return e, M
+        
+
+
+def gen_grouped_eigenpairs(edirs,Mdirs,lbls,efilename_templates, Mfilename_templates,remove_redundant=True):
     energies_gen = [gen_energies(edir, lbls,efnt) for (edir, efnt) in zip(edirs, efilename_templates)]
     mos_gen = [gen_mos(Mdir, lbls, mfnt) for (Mdir, mfnt) in zip(edirs, Mfilename_templates)]
-    for nn in lbls:
+    nlbls = range(len(lbls))
+
+    for n in range(lbls):
         energies = np.hstack([egen.next() for egen in energies_gen])
         MOs = np.hstack([mgen.next() for mgen in mos_gen])
-        isorted = np.argsort(energies)
-        energies = energies[isorted]
-        MOs = MOs[:,isorted]
+        if remove_redundant:
+            energies, MOs = remove_redundant_eigenpairs(energies, MOs)
+        else:
+            isorted = np.argsort(energies)
+            energies = energies[isorted]
+            MOs = MOs[:,isorted]
 
         yield energies, MOs
